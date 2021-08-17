d5f5be4ec20923dcc97a3b1e383f46567fcc0c63
leellou <lea.destaillac@gmail.com>
2021-08-17 19:35:02 +0200

    seed users & players

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+
+Player.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+
+User.create(email: "contact@eiffy.fr", password: "blacklist")
+User.create(email: "contact@nymph.fr", password: "blacklist")
+
+puts "Creating players..."
+
+Player.create(name: "Ei'ffy Khas", server: "Spriggan")
+Player.create(name: "Nymph Astadeviluke", server: "Cerberus")
+Player.create(name: "Gekido Mazoku", server: "Ragnarok")
+Player.create(name: "Chizu Kha", server: "Moogle")
+Player.create(name: "O'ra Nunh", server: "Omega")
+Player.create(name: "Shaoka Khao", server: "Moogle")
+Player.create(name: "Beh Vitrey", server: "Moogle")
+Player.create(name: "Cheria Mishima", server: "Cerberus")
+Player.create(name: "Aerhast Oschon", server: "Louisoix")
+Player.create(name: "Cordelia Nekko", server: "Louisoix")
+
+puts "Finished!"
